1240faf9a849a662a5e0e2c81dcefbabc49fe0f5
AndrewTheDrune <154268877+AndrewTheDrune@users.noreply.github.com>
2024-04-15 01:49:01 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -7,61 +7,85 @@ const button = document.querySelector("button");
 const inputMain = document.querySelector("input");
 const divMain = document.querySelector(".main-background");
 const ul = document.createElement("ul");
+// let liObject = {
+//     li: HTMLLIElement,
+//     input: HTMLInputElement,
+//     buttonDone: HTMLButtonElement,
+//     buttonDelete: HTMLButtonElement
+// };
 
-button.disabled = true;
-inputMain.addEventListener("input", function(){
-    button.disabled = false;
-})
-
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    let li = createListItem();
-    let input = createInput();
-    let buttonDone = createButtonDone();
-    let buttonDelete = createButtonDelete();
-
+function main(){
     button.disabled = true;
     inputMain.addEventListener("input", function(){
         button.disabled = false;
     })
 
-    if (!inputMain.value.trim()) {
-        alert("Нужно ввести название дела");
-    }
-    else{    
-        divMain.append(ul);
-        ul.append(li);
-        li.append(input);
-        li.append(buttonDone);
-        li.append(buttonDelete);
-        input.value = inputMain.value.trim();    
-    }
+    button.addEventListener("click", function(event) {
+        event.preventDefault();
 
-    inputMain.value = null;
+        let li = createListItem();
+        let input = createInput();
+        let buttonDone = createButtonDone();
+        let buttonDelete = createButtonDelete();
 
-    buttonDone.addEventListener("click", function(){
-        if (li.style.backgroundColor == "lightgreen") {
-            buttonDone.innerText = "Не готово";
-            li.style.backgroundColor = "rgb(192, 209, 224)";
+        button.disabled = true;
+        inputMain.addEventListener("input", function(){
+            button.disabled = false;
+        })
+
+        if (!inputMain.value.trim()) {
+            alert("Нужно ввести название дела");
         }
-        else{
-            li.style.backgroundColor = "lightgreen";
-            buttonDone.innerText = "Готово";
+        else{    
+            divMain.append(ul);
+            ul.append(li);
+            li.append(input);
+            li.append(buttonDone);
+            li.append(buttonDelete);
+            input.value = inputMain.value.trim();    
         }
+
+        inputMain.value = null;
+
+        buttonDone.addEventListener("click", function(){
+            if (li.style.backgroundColor == "lightgreen") {
+                buttonDone.innerText = "Не готово";
+                li.style.backgroundColor = "rgb(192, 209, 224)";
+            }
+            else{
+                li.style.backgroundColor = "lightgreen";
+                buttonDone.innerText = "Готово";
+            }
+        })
+
+        buttonDelete.addEventListener("click", function(){
+            if(confirm("Вы уверены?")){
+                li.remove();             
+            }
+        })
+
+        // liObject.li = li;
+        // liObject.input = input;
+        // liObject.buttonDone = buttonDone;
+        // liObject.buttonDelete = buttonDelete;
+
+        let liObject = {
+            li: li,
+            input: input,
+            buttonDone: buttonDone,
+            buttonDelete: buttonDelete
+        };
+
+        localStorage.setItem("list", JSON.stringify(liObject));
     })
+}
 
-    buttonDelete.addEventListener("click", function(){
-        if(confirm("Вы уверены?")){
-            li.remove();             
-        }
-    })
+main();
 
-    localStorage.setItem("list", JSON.stringify(input.value));
-
-
-})
 document.addEventListener("DOMContentLoaded", function(){
-    localStorage.getItem("list", JSON.parse(input.value));
+    let obj = localStorage.getItem("list");
+    let liObject = JSON.parse(obj);
 
+    console.log(liObject);
+    ul.append(liObject.li)
 })
